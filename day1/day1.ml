04c2491abe902f6f3b1970db3be77c2c9e44e81d
Filip Kučerák <xkucerak@fi.muni.cz>
2021-12-03 14:28:38 +0100

    center

--- day1/day1.ml
+++ day1/day1.ml
@@ -1,33 +1,40 @@
-let read_line stdin = 
-    try Some (input_line stdin) with 
-        End_of_file -> None;;
+                                       let read_line stdin = 
+                                  try Some (input_line stdin) with 
+                                       End_of_file -> None;;
 
-let process_stdin tran stdin = 
-    let rec input lines =
-        match read_line stdin with 
-            Some line -> input ((tran line) :: lines)
-        |   None -> List.rev lines 
-    in 
-    input [];;
+                                   let process_stdin tran stdin = 
+                                       let rec input lines =
+                                     match read_line stdin with 
+                             Some line -> input ((tran line) :: lines)
+                                     |   None -> List.rev lines 
+                                                 in 
+                                             input [];;
 
-let print_int_list l = List.map string_of_int l |> String.concat "\n" |> print_string
+                                       let print_int_list l = 
+                                      List.map string_of_int l 
+                                       |> String.concat "\n" 
+                                          |> print_string
 
-let maybe_greater a b = 
-  match b with
-    | None -> false 
-    | Some n -> a > n 
+                                      let maybe_greater a b = 
+                                            match b with
+                                          | None -> false 
+                                         | Some n -> a > n 
 
 
-let rec count_growing last numbers =
-  match numbers with
-    | [] -> 0 
-    | h :: t -> (if maybe_greater h last then 1 else 0) + count_growing (Some h) t
+                                let rec count_growing last numbers =
+                                         match numbers with
+                                             | [] -> 0 
+           | h :: t -> (if maybe_greater h last then 1 else 0) + count_growing (Some h) t
 
-let rec merge numbers = 
-  match numbers with 
-    | a :: b :: c :: t -> (a + b + c) :: (merge (b :: c :: t))
-    | _ -> [] 
+                                      let rec merge numbers = 
+                                         match numbers with 
+                     | a :: b :: c :: t -> (a + b + c) :: (merge (b :: c :: t))
+                                             | _ -> [] 
 
-let () = (open_in "input.txt") |> process_stdin int_of_string |> merge |> count_growing None |> string_of_int |> print_endline
-
-    (*|> count_growing None |> string_of_int |> print_endline*)
+                                              let () = 
+                                       (open_in "input.txt") 
+                                   |> process_stdin int_of_string 
+                                              |> merge 
+                                       |> count_growing None 
+                                          |> string_of_int 
+                                          |> print_endline
--- day2/day2.ml
+++ day2/day2.ml
@@ -1,33 +1,35 @@
-open Format
+                                            open Format
 
-let read_line stdin = 
-    try Some (input_line stdin) with 
-        End_of_file -> None;;
+                                       let read_line stdin = 
+                                  try Some (input_line stdin) with 
+                                       End_of_file -> None;;
 
-let build_tran tran line_tran acc line = tran acc @@ line_tran line
+                let build_tran tran line_tran acc line = tran acc @@ line_tran line
 
-let rec stdin_fold tran start stdin = match read_line stdin with
-    |   Some line -> stdin_fold tran (tran start line) stdin
-    |   None -> start
+                  let rec stdin_fold tran start stdin = match read_line stdin with
+                      |   Some line -> stdin_fold tran (tran start line) stdin
+                                         |   None -> start
 
-let parse_line line = match String.split_on_char(' ') line with 
-    | dir::loc::_ -> dir, int_of_string loc
-    | _ -> "", 0
+                  let parse_line line = match String.split_on_char(' ') line with 
+                              | dir::loc::_ -> dir, int_of_string loc
+                                            | _ -> "", 0
 
-let move (pos, depth) (dir, x) =
-    match dir with 
-    |   "forward" -> pos + x, depth
-    |   "up" -> pos, depth - x
-    |   "down" -> pos, depth + x
-    |   _ -> pos, depth
+                                  let move (pos, depth) (dir, x) =
+                                           match dir with 
+                                  |   "forward" -> pos + x, depth
+                                     |   "up" -> pos, depth - x
+                                    |   "down" -> pos, depth + x
+                                        |   _ -> pos, depth
 
-let move2 (pos, depth, aim) (dir, x) =
-    match dir with 
-    |   "forward" -> pos + x, depth + aim * x, aim
-    |   "up" -> pos, depth, aim - x
-    |   "down" -> pos, depth, aim + x
-    |   _ -> pos, depth, aim
+                               let move2 (pos, depth, aim) (dir, x) =
+                                           match dir with 
+                           |   "forward" -> pos + x, depth + aim * x, aim
+                                  |   "up" -> pos, depth, aim - x
+                                 |   "down" -> pos, depth, aim + x
+                                      |   _ -> pos, depth, aim
 
-let () = open_in "input.txt" |> stdin_fold (build_tran move2 parse_line) (0, 0, 0) 
-        |> function p, d, _ -> print_int (p * d)
+                                              let () = 
+                                        open_in "input.txt" 
+                       |> stdin_fold (build_tran move2 parse_line) (0, 0, 0) 
+                              |> function p, d, _ -> print_int (p * d)
 
